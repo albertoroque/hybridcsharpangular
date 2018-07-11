8a0aee5683b1d9cb8ac239fa19c36c68d92c1f71
Alberto Roque <alberto.roque097@hotmail.com>
2018-07-11 16:10:06 -0300

    atualização dos arquivos

--- HybridAngularCsharp/Controllers/HomeController.cs
+++ HybridAngularCsharp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HybridAngularCsharp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,25 +7,79 @@ using System.Web.Mvc;
 
 namespace HybridAngularCsharp.Controllers
 {
+    [RoutePrefix("")]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        string view = "~/Views/Home/Index.cshtml";
+
+
+        [Route("~/")]
+        public ActionResult Home()
         {
-            return View();
+
+            var model = new OpenGraph
+            {
+                title = "Catáloko",
+                description = "Teste da home",
+                image = "https://assets.nike.com.br/ArquivoExibir/1148275.jpg"
+            };
+
+            ViewBag.OpenGraph = model;
+
+            return View(view);
+        }
+      
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [Route("~/e")]
+        public ActionResult Error()
+        {
+            var r = Request.QueryString["aspxerrorpath"];
+            return Redirect("#" + r);
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
+        #region Notícia
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Route("~/n/{title}/{id:long:min(1)}")]
+        public ActionResult Noticia(string title, long id)
+        {
+
+            var model = new OpenGraph
+            {
+                title = "Noticia",
+                description = "Teste de noticia 2018 será lançado em breve",
+                image = "https://assets.nike.com.br/ArquivoExibir/1148275.jpg"
+            };
+
+            ViewBag.OpenGraph = model;
+
+            return View(view);
         }
 
-        public ActionResult Contact()
+        [Route("~/noticias")]
+        public ActionResult Noticias()
         {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+            var model = new OpenGraph
+            {
+                title = "Notícias",
+                description = "Lista de notícias",
+                image = "https://assets.nike.com.br/ArquivoExibir/1148275.jpg"
+            };
+
+            ViewBag.OpenGraph = model;
+
+            return View(view);
         }
+
+        #endregion
     }
 }
---
+++ HybridAngularCsharp/Models/OpenGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HybridAngularCsharp.Models
+{
+    public class OpenGraph
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string image { get; set; }
+    }
+}
